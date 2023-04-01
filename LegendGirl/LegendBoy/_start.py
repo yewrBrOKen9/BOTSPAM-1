ecf249f1deb5ac6e89477b9ad53501c4b61cefad
adarshUC <121399431+adarshUC@users.noreply.github.com>
2023-04-01 15:34:02 +0530

    Update _start.py

--- LegendGirl/LegendBoy/_start.py
+++ LegendGirl/LegendBoy/_start.py
@@ -1,36 +1,44 @@
-from LegendBS.start import start_cmd
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, Message
+from telethon import events, Button
+from config import MK1, MK2, MK3, MK4, MK5, MK6, MK7, MK8, MK9, MK10
+from AltronX.modules.help import *
+import telethon
 
-from LegendGirl.Config import *
+PythonButton = [
+        [
+        Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
+        ],
+        [
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/theBROKENXD"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/theBROKEN_X_SUPPORT")
+        ],
+        [
+        Button.url("• ʀᴇᴘᴏ •", "https://te.legra.ph/file/fd4d39b5f02deebc95627.mp4")
+        ]
+        ]
 
-from ..core.clients import *
 
-
-@Client.on_message(filters.command(["start"], prefixes=HANDLER))
-async def start(Legend: Client, message: Message):
-    if ".jpg" in START_PIC or ".png" in START_PIC:
-        for i in range(1, 26):
-            lol = globals()[f"Client{i}"]
-            if lol is not None:
-                await lol.send_photo(
-                    message.chat.id,
-                    START_PIC,
-                    caption=START_MESSAGE,
-                    reply_markup=InlineKeyboardMarkup(await start_cmd(Legend)),
-                )
-    elif ".mp4" in START_PIC.lower():
-        for i in range(1, 26):
-            lol = globals()[f"Client{i}"]
-            if lol is not None:
-                await lol.send_video(
-                    message.chat.id,
-                    START_PIC,
-                    caption=START_MESSAGE,
-                    reply_markup=InlineKeyboardMarkup(await start_cmd(Legend)),
-                )
-    else:
-        for i in range(1, 26):
-            lol = globals()[f"Client{i}"]
-            if lol is not None:
-                await lol.send_message(message.chat_id, START_MESSAGE)
+@MK1.on(events.NewMessage(pattern="/start"))
+@MK2.on(events.NewMessage(pattern="/start"))
+@MK3.on(events.NewMessage(pattern="/start"))
+@MK4.on(events.NewMessage(pattern="/start"))
+@MK5.on(events.NewMessage(pattern="/start"))
+@MK6.on(events.NewMessage(pattern="/start"))
+@MK7.on(events.NewMessage(pattern="/start"))
+@MK7.on(events.NewMessage(pattern="/start"))
+@MK8.on(events.NewMessage(pattern="/start"))
+@MK9.on(events.NewMessage(pattern="/start"))
+@MK10.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+        AltBot = await event.client.get_me()
+        BotName = AltBot.first_name
+        BotId = AltBot.id
+        TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{BotName}](tg://user?id={BotId})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [✘](https://t.me/Dead_HeartLess)**\n\n"
+        TEXT += f"» **ʙᴏᴛ ꜱᴘᴀᴍ ᴠᴇʀsɪᴏɴ :** `M3.2`\n"
+        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
+        await event.client.send_file(
+                event.chat_id,
+                "https://te.legra.ph/file/75d174dd53b3e5755d033.jpg",
+                caption=TEXT, 
+                buttons=PythonButton)
